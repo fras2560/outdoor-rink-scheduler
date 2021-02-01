749b5d08b5daa257133f568274189bd4636286f9
fras2560 <dallas.fraser.waterloo@gmail.com>
2021-01-31 20:03:06 -0500

    Add a privacy policy page and terms of condition page
    Additionally fix the home link

--- app/views/static.py
+++ app/views/static.py
@@ -8,3 +8,15 @@ def about_page():
     return render_template("about.html",
                            page="page",
                            title="About the app")
+
+
+@current_app.route("/privacy")
+def privacy_policy():
+    """A route for the privacy policy."""
+    return render_template("privacy_policy.html")
+
+
+@current_app.route("/terms-and-conditions")
+def terms_and_conditions():
+    """A route for the terms and conditions."""
+    return render_template("terms_and_conditions.html")
